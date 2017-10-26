fbd7e9c40339c94678d34ab30bad876b0f4650ba
Lucie Kureckova <xkureck@fi.muni.cz>
2017-10-26 22:00:08 +0200

    add atribut developers to Car class

--- src/main/java/cz/muni/fi/dao/TeamDao.java
+++ src/main/java/cz/muni/fi/dao/TeamDao.java
@@ -3,11 +3,15 @@ package cz.muni.fi.dao;
 import cz.muni.fi.entities.Car;
 
 /**
- *
+ * 
  * @author Lucie Kureckova, 445264
  */
 public interface TeamDao {
     
+    /**
+     * 
+     * @param car 
+     */
     public void changeFirstCar(Car car);
     
     public void changeSecondCar(Car car);
