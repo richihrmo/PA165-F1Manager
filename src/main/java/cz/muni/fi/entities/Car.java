93ab715492ddf1c54ab3f4c2b1505731b7e77ac2
Lucie Kureckova <xkureck@fi.muni.cz>
2017-10-26 21:31:56 +0200

    add hashcode, Equals and setters throw exception in class Car

--- src/main/java/cz/muni/fi/entities/Car.java
+++ src/main/java/cz/muni/fi/entities/Car.java
@@ -1,5 +1,8 @@
 package cz.muni.fi.entities;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * Class represents car
  * @author Lucie Kureckova, 445264
@@ -10,19 +13,19 @@ public class Car {
     
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private Long id;
+    @Getter private Long id;
     @OneToOne
-    private Person driver;
+    @Setter @Getter private Person driver;
     @OneToOne
-    private Component engine;
+    @Getter private Component engine;
     @OneToOne
-    private Component aerodynamics;
+    @Getter private Component aerodynamics;
     @OneToOne
-    private Component suspension;
+    @Getter private Component suspension;
     @OneToOne
-    private Component transmission;
+    @Getter private Component transmission;
     @OneToOne
-    private Component brakes;
+    @Getter private Component brakes;
 
     public Car(Person driver, Component engine, Component aerodynamics, Component transmission, Component brakes) {
         this.driver = driver;
@@ -32,52 +35,88 @@ public class Car {
         this.brakes = brakes;
     }
 
-    public Person getDriver() {
-        return driver;
-    }
-
-    public void setDriver(Person driver) {
-        this.driver = driver;
-    }
-
-    public Component getEngine() {
-        return engine;
-    }
-
     public void setEngine(Component engine) {
+        if(engine.getType() != ComponentType.ENGINE){
+            throw new IllegalArgumentException("type of component is not ENGINE");
+        }
         this.engine = engine;
     }
 
-    public Component getAerodynamics() {
-        return aerodynamics;
-    }
-
     public void setAerodynamics(Component aerodynamics) {
+        if(aerodynamics.getType() != ComponentType.AERODYNAMICS){
+            throw new IllegalArgumentException("type of component is not AERODYNAMICS");
+        }
         this.aerodynamics = aerodynamics;
     }
 
-    public Component getSuspension() {
-        return suspension;
-    }
-
     public void setSuspension(Component suspension) {
+        if(suspension.getType() != ComponentType.SUSPENSION){
+            throw new IllegalArgumentException("type of component is not SUSPENSION");
+        }
         this.suspension = suspension;
     }
 
-    public Component getTransmission() {
-        return transmission;
-    }
-
     public void setTransmission(Component transmission) {
+        if(transmission.getType() != ComponentType.TRANSMISSION){
+            throw new IllegalArgumentException("type of component is not TRANSMISSION");
+        }
         this.transmission = transmission;
     }
 
-    public Component getBrakes() {
-        return brakes;
-    }
-
     public void setBrakes(Component brakes) {
+        if(brakes.getType() != ComponentType.BRAKES){
+            throw new IllegalArgumentException("type of component is not BRAKES");
+        }
         this.brakes = brakes;
     }
-    
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 97 * hash + (this.id != null ? this.id.hashCode() : 0);
+        hash = 97 * hash + (this.driver != null ? this.driver.hashCode() : 0);
+        hash = 97 * hash + (this.engine != null ? this.engine.hashCode() : 0);
+        hash = 97 * hash + (this.aerodynamics != null ? this.aerodynamics.hashCode() : 0);
+        hash = 97 * hash + (this.suspension != null ? this.suspension.hashCode() : 0);
+        hash = 97 * hash + (this.transmission != null ? this.transmission.hashCode() : 0);
+        hash = 97 * hash + (this.brakes != null ? this.brakes.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof Car)) {
+            return false;
+        }
+        final Car other = (Car) obj;
+        if (this.id != other.id && (this.id == null || !this.id.equals(other.id))) {
+            return false;
+        }
+        if (this.driver != other.driver && (this.driver == null || !this.driver.equals(other.driver))) {
+            return false;
+        }
+        if (this.engine != other.engine && (this.engine == null || !this.engine.equals(other.engine))) {
+            return false;
+        }
+        if (this.aerodynamics != other.aerodynamics && (this.aerodynamics == null || !this.aerodynamics.equals(other.aerodynamics))) {
+            return false;
+        }
+        if (this.suspension != other.suspension && (this.suspension == null || !this.suspension.equals(other.suspension))) {
+            return false;
+        }
+        if (this.transmission != other.transmission && (this.transmission == null || !this.transmission.equals(other.transmission))) {
+            return false;
+        }
+        if (this.brakes != other.brakes && (this.brakes == null || !this.brakes.equals(other.brakes))) {
+            return false;
+        }
+        return true;
+    }
+ 
 }
