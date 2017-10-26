96c0f2966fbe524a9741ed7cdbb8af3b52d5cdd5
robert.tamas <robert.tamas@roihunter.com>
2017-10-26 20:38:17 +0200

    Car dao interface with implementation

---
+++ src/main/java/cz/muni/fi/dao/CarDao.java
@@ -0,0 +1,55 @@
+package cz.muni.fi.dao;
+
+import cz.muni.fi.entities.Driver;
+
+import java.util.List;
+
+/**
+ * @author Robert Tamas
+ */
+public interface CarDao {
+
+    /**
+     * List all cars which are in DB
+     *
+     * @return list of cars
+     */
+    public List<Car> listAllCars();
+
+    /**
+     * Find specific car by its driver
+     *
+     * @param driver driver
+     * @return car with driver
+     */
+    public Car findCar(Driver driver);
+
+    /**
+     * Find specific car by its id
+     *
+     * @param id DB id
+     * @return car
+     */
+    public Car findCar(Long id);
+
+    /**
+     * Adds car to DB
+     *
+     * @param car car to be added
+     */
+    public void addCar(Car car);
+
+    /**
+     * Updates car in DB
+     *
+     * @param car car to be updated
+     */
+    public void updateCar(Car car);
+
+    /**
+     * Deletes car from DB
+     *
+     * @param car car to be deleted
+     */
+    public void deleteCar(Car car);
+}
---
+++ src/main/java/cz/muni/fi/dao/CarDaoImpl.java
@@ -0,0 +1,50 @@
+package cz.muni.fi.dao;
+
+import cz.muni.fi.entities.Driver;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+
+import java.util.List;
+
+
+/**
+ * @auhor Robert Tamas
+ */
+@Repository
+public class CarDaoImpl implements CarDao{
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public List<Car> listAllCars() {
+        return em.createQuery("select c from Car c", Car.class).getResultList();
+    }
+
+    public Car findCar(Driver driver) {
+        try {
+            return em.createQuery("select c from Car c where c.driver = :driver", Car.class)
+                    .setParameter("driver", driver).getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
+
+    public Car findCar(Long id) {
+        return em.find(Car.class, id);
+    }
+
+    public void addCar(Car car) {
+        em.persist(car);
+    }
+
+    public void updateCar(Car car) {
+        em.merge(car);
+    }
+
+    public void deleteCar(Car car) {
+        em.remove(em.merge(car));
+    }
+}
