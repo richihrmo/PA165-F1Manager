c5189ebc2046ad669df3a4f2d007f29115699b24
Richard Hrmo <xhrmo@fi.muni.cz>
2017-10-29 00:57:06 +0200

    update PR
    (cherry picked from commit 8d03db7)

--- src/main/java/cz/muni/fi/ComponentType.java
+++ src/main/java/cz/muni/fi/ComponentType.java
@@ -4,5 +4,5 @@ package cz.muni.fi;
  * @author Richard Hrmo
  */
 public enum ComponentType{
-    ENGINE, TRANSMISSION, BRAKES, AERODYNAMICS, SUSPENSION
+    ENGINE, TRANSMISSION, BRAKES, AERO, BODY
 }
