21d395db74ab7e1c1ea83741df2285513ec5e6bd
Matus Macko <matus.macko@vacuumlabs.com>
2017-11-26 15:17:48 +0100

    Add service and facade layer

--- service/src/main/java/cz/muni/fi/service/TeamService.java
+++ service/src/main/java/cz/muni/fi/service/TeamService.java
@@ -1,4 +1,54 @@
 package cz.muni.fi.service;
 
+import java.util.List;
+
+import cz.muni.fi.entities.Team;
+import cz.muni.fi.entities.Driver;
+
+/**
+ * @author Matus Macko
+ */
 public interface TeamService {
+    /**
+     * Find team by id
+     *
+     * @param id team id
+     * @return found team
+     */
+    Team findTeamById(Long id);
+
+    /**
+     * Find all teams
+     *
+     * @return list of found teams
+     */
+    List<Team> findAllTeams();
+
+    /**
+     * Find all team drivers
+     *
+     * @return list of team drivers
+     */
+    List<Driver> findAllTeamCarDrivers();
+
+    /**
+     * Create new team
+     *
+     * @param team team to be created
+     */
+    void createTeam(Team team);
+
+    /**
+     * Update team
+     *
+     * @param team team to be updated
+     */
+    void updateTeam(Team team);
+
+    /**
+     * Delete team
+     *
+     * @param team team to be updated
+     */
+    void deleteTeam(Team team);
 }
---
+++ service/src/main/java/cz/muni/fi/service/facade/TeamFacadeImpl.java
@@ -0,0 +1,61 @@
+package cz.muni.fi.service.facade;
+
+import cz.muni.fi.dto.DriverDTO;
+import cz.muni.fi.dto.NewTeamDTO;
+import cz.muni.fi.dto.TeamDTO;
+import cz.muni.fi.entities.Team;
+import cz.muni.fi.facade.TeamFacade;
+import cz.muni.fi.service.BeanMappingService;
+import cz.muni.fi.service.TeamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.inject.Inject;
+import java.util.List;
+
+/**
+ * @author Matus Macko
+ */
+@Service
+@Transactional
+public class TeamFacadeImpl implements TeamFacade{
+    @Inject
+    private TeamService teamService;
+
+    @Autowired
+    private BeanMappingService beanMappingService;
+
+    @Override
+    public List<TeamDTO> getAllTeams() {
+        return beanMappingService.mapTo(teamService.findAllTeams(), TeamDTO.class);
+    }
+
+    @Override
+    public TeamDTO getTeamById(Long teamId) {
+        return beanMappingService.mapTo(teamService.findTeamById(teamId), TeamDTO.class);
+    }
+
+    @Override
+    public void createTeam(NewTeamDTO team) {
+        Team mappedTeam = beanMappingService.mapTo(team, Team.class);
+        teamService.createTeam(mappedTeam);
+    }
+
+    @Override
+    public void updateTeam(TeamDTO team) {
+        Team mappedTeam = beanMappingService.mapTo(team, Team.class);
+        teamService.updateTeam(mappedTeam);
+    }
+
+    @Override
+    public void deleteTeam(TeamDTO team) {
+        Team mappedTeam = beanMappingService.mapTo(team, Team.class);
+        teamService.deleteTeam(mappedTeam);
+    }
+
+    @Override
+    public List<DriverDTO> getAllTeamCarDrivers() {
+        return beanMappingService.mapTo(teamService.findAllTeamCarDrivers(), DriverDTO.class);
+    }
+}
