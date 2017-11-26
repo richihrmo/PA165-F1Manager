3eebe44485ed571514fc775f0cb45acaab5523d0
Lucie Kureckova <xkureck@fi.muni.cz>
2017-11-26 18:01:24 +0100

    service layer for Driver and related test

--- service/src/main/java/cz/muni/fi/service/DriverService.java
+++ service/src/main/java/cz/muni/fi/service/DriverService.java
@@ -1,4 +1,73 @@
 package cz.muni.fi.service;
 
+import cz.muni.fi.entities.Driver;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Lucie Kureckova, 445264
+ */
+@Service
 public interface DriverService {
+    
+    /**
+     * Find all drivers
+     * 
+     * @return list of drivers
+     */
+    List<Driver> listDrivers();
+    
+    /**
+     * Find all test drivers
+     * 
+     * @return list of test drivers
+     */
+    List<Driver> listTestDrivers();
+    
+    /**
+     * Find driver by id
+     * 
+     * @param id of wanted driver
+     * @return wanted driver
+     */
+    Driver findDriverById(Long id);
+    
+    /**
+     * Find driver by name
+     * 
+     * @param name of driver
+     * @param surname of driver
+     * @return wanted driver
+     */
+    Driver findDriverByName(String name, String surname);
+    
+    /**
+     * Find test driver by name
+     * 
+     * @param name of driver
+     * @param surname of driver
+     * @return wanted driver
+     */
+    Driver findTestDriver(String name, String surname);
+    
+    /**
+     * Add driver
+     * 
+     * @param driver to add
+     */
+    void addDriver(Driver driver);
+    
+    /**
+     * Update driver
+     * 
+     * @param driver to update
+     */
+    void updateDriver(Driver driver);
+    
+    /**
+     * Delete driver
+     * 
+     * @param driver to delete
+     */
+    void deleteDriver(Driver driver);
 }
