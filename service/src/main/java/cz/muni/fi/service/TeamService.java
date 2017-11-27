cf1f806fd4fb12a54141fcef06e17a56b2104bdd
Matus Macko <matus.macko@vacuumlabs.com>
2017-11-27 16:58:01 +0100

    Delete TeamFormulaDataAccessException

--- service/src/main/java/cz/muni/fi/service/TeamService.java
+++ service/src/main/java/cz/muni/fi/service/TeamService.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import cz.muni.fi.entities.Team;
 import cz.muni.fi.entities.Driver;
-import cz.muni.fi.service.exceptions.TeamFormulaDataAccessException;
 
 /**
  * @author Matus Macko
@@ -16,7 +15,6 @@ public interface TeamService {
      * @param id team id
      * @return found team
      * @throws IllegalArgumentException if id argument id null
-     * @throws TeamFormulaDataAccessException if case of error on data access layer
      */
     Team findTeamById(Long id);
 
@@ -24,7 +22,6 @@ public interface TeamService {
      * Find all teams
      *
      * @return list of found teams
-     * @throws TeamFormulaDataAccessException if case of error on data access layer
      */
     List<Team> findAllTeams();
 
@@ -32,7 +29,6 @@ public interface TeamService {
      * Find all team drivers
      *
      * @return list of team drivers
-     * @throws TeamFormulaDataAccessException if case of error on data access layer
      */
     List<Driver> findAllTeamCarDrivers();
 
@@ -42,7 +38,6 @@ public interface TeamService {
      * @param team team to be created
      * @return created Team
      * @throws IllegalArgumentException if team argument id null
-     * @throws TeamFormulaDataAccessException if case of error on data access layer
      */
     Team createTeam(Team team);
 
@@ -52,7 +47,6 @@ public interface TeamService {
      * @param team team to be updated
      * @return updated Team
      * @throws IllegalArgumentException if team argument id null
-     * @throws TeamFormulaDataAccessException if case of error on data access layer
      */
     Team updateTeam(Team team);
 
@@ -61,7 +55,6 @@ public interface TeamService {
      *
      * @param team team to be updated
      * @throws IllegalArgumentException if team argument id null
-     * @throws TeamFormulaDataAccessException if case of error on data access layer
      */
     void deleteTeam(Team team);
 }
