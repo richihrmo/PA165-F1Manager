8eb3fa19d28f6bce24464f7b4078a348f8e5783a
robert.tamas <robert.tamas@roihunter.com>
2017-11-27 02:20:10 +0100

    Component facade refactor, implementation and tests

--- api/src/main/java/cz/muni/fi/dto/ComponentDTO.java
+++ api/src/main/java/cz/muni/fi/dto/ComponentDTO.java
@@ -12,7 +12,24 @@ public class ComponentDTO {
     @Getter @Setter private Long id;
     @Getter @Setter private String name;
     @Getter @Setter private ComponentType type;
-    @Getter @Setter private Boolean isAvailable;
+    @Getter @Setter private Boolean availability;
+
+    public ComponentDTO () {}
+
+    public ComponentDTO(String name, Boolean isAvailable, ComponentType type) {
+        this.name = name;
+        this.type = type;
+        this.availability = isAvailable;
+    }
+
+    @Override
+    public String toString() {
+        return "ComponentDTO{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", type=" + type +
+                '}';
+    }
 
     @Override
     public boolean equals(Object o) {
