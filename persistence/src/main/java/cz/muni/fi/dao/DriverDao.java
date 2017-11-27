ab2789607845444359919bcf898ba72e5f66ecee
Lucie Kureckova <xkureck@fi.muni.cz>
2017-11-27 22:57:05 +0100

    fixing

--- persistence/src/main/java/cz/muni/fi/dao/DriverDao.java
+++ persistence/src/main/java/cz/muni/fi/dao/DriverDao.java
@@ -37,7 +37,7 @@ public interface DriverDao{
      * @param name driver's name
      * @param surname driver's surname
      * @return Driver
-     * @throws IllegalArgumentException when argumet is null
+     * @throws IllegalArgumentException when some argumet is null
      */
     public Driver findDriverByName(String name, String surname);
 
@@ -47,7 +47,7 @@ public interface DriverDao{
      * @param name test driver's name
      * @param surname test driver's surname
      * @return Driver
-     * @throws IllegalArgumentException when argumet is null
+     * @throws IllegalArgumentException when some argumet is null
      */
     public Driver findTestDriver(String name, String surname);
 
@@ -56,7 +56,8 @@ public interface DriverDao{
      *
      * @param driver Driver
      * @return Driver
-     * @throws IllegalArgumentException when argumet is null
+     * @throws IllegalArgumentException when argument is null, or not nullable attributes are null,
+     *         or id is not null
      */
     public Driver addDriver(Driver driver);
 
@@ -65,7 +66,7 @@ public interface DriverDao{
      *
      * @param driver Driver
      * @return Driver
-     * @throws IllegalArgumentException when argumet is null
+     * @throws IllegalArgumentException when argument is null or not nullable component attributes are null
      */
     public Driver updateDriver(Driver driver);
 
@@ -74,7 +75,7 @@ public interface DriverDao{
      *
      * @param driver Driver
      * @return Driver
-     * @throws IllegalArgumentException when argumet is null
+     * @throws IllegalArgumentException when argument is null or not nullable component attributes are null
      */
     public Driver deleteDriver(Driver driver);
 
