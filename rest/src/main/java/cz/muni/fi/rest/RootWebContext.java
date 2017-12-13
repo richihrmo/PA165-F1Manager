8918b6ac4a5a7aef0159208998ce8c2116417862
Matus Macko <matus.macko@vacuumlabs.com>
2017-12-13 22:22:24 +0100

    Add basic folder structure for rest and mvc

---
+++ rest/src/main/java/cz/muni/fi/rest/RootWebContext.java
@@ -0,0 +1,58 @@
+package cz.muni.fi.rest;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import cz.muni.fi.service.config.ServiceConfiguration;
+import org.springframework.context.annotation.*;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import cz.muni.fi.TeamFormula1SampleDataConfiguration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Locale;
+
+@EnableWebMvc
+@Configuration
+@Import({ServiceConfiguration.class, TeamFormula1SampleDataConfiguration.class})
+@ComponentScan(basePackages = {"cz.muni.fi.rest.controllers"})
+public class RootWebContext extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AllowOriginInterceptor());
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Bean
+    @Primary
+    public MappingJackson2HttpMessageConverter customJackson2HttpMessageConverter() {
+        MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.ENGLISH));
+
+
+        objectMapper.disable(MapperFeature.DEFAULT_VIEW_INCLUSION);
+
+        jsonConverter.setObjectMapper(objectMapper);
+        return jsonConverter;
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(customJackson2HttpMessageConverter());
+    }
+
+}
---
+++ sample-data/src/main/java/cz/muni/fi/TeamFormula1SampleDataConfiguration.java
@@ -0,0 +1,31 @@
+package cz.muni.fi;
+
+import java.io.IOException;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import cz.muni.fi.service.config.ServiceConfiguration;
+
+@Configuration
+@Import(ServiceConfiguration.class)
+@ComponentScan(basePackageClasses = {SampleDataLoadingFacadeImpl.class})
+public class TeamFormula1SampleDataConfiguration {
+
+    final static Logger log = LoggerFactory.getLogger(TeamFormula1SampleDataConfiguration.class);
+
+    @Autowired
+    SampleDataLoadingFacade sampleDataLoadingFacade;
+
+    @PostConstruct
+    public void dataLoading() throws IOException {
+        //log.debug("dataLoading()");
+        //sampleDataLoadingFacade.loadData();
+    }
+
+}
