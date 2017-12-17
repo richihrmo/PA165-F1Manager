8de72422fc5aefab753412d355d7bda6689c3d4e
robert.tamas01@gmail.com <xtamas@fi.muni.cz>
2017-12-17 12:48:01 +0100

    Enum edited for mvc

--- api/src/main/java/cz/muni/fi/enums/ComponentType.java
+++ api/src/main/java/cz/muni/fi/enums/ComponentType.java
@@ -1,8 +1,29 @@
 package cz.muni.fi.enums;
 
+import lombok.Getter;
+
 /**
  * @author Robert Tamas
  */
 public enum ComponentType {
-    ENGINE, TRANSMISSION, BRAKES, AERODYNAMICS, SUSPENSION
+    ENGINE("ENGINE", "engine"),
+    TRANSMISSION("TRANSMISSION", "transmission"),
+    BRAKES("BRAKES", "brakes"),
+    AERODYNAMICS("AERODYNAMICS", "aerodynamics"),
+    SUSPENSION("SUSPENSION", "suspension");
+
+    @Getter private String name;
+    @Getter private String urlAnnotation;
+
+    ComponentType(String name, String urlAnnotation) {
+        this.name = name;
+        this.urlAnnotation = urlAnnotation;
+    }
+
+    public static ComponentType parse(String annotation) {
+        for (ComponentType type : values()) {
+            if (type.getUrlAnnotation().equals(annotation)) return type;
+        }
+        return null;
+    }
 }
