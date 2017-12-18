b4645e066a30cd72bd62dfb03e8bb58e7c7cb4e9
robert.tamas01@gmail.com <xtamas@fi.muni.cz>
2017-12-18 22:53:19 +0100

    Easter egg

--- sample-data/src/main/java/cz/muni/fi/SampleDataLoadingFacadeImpl.java
+++ sample-data/src/main/java/cz/muni/fi/SampleDataLoadingFacadeImpl.java
@@ -15,7 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
